332fda0c453d73373f43bae552d27a7030500024
Y4h14 <yahyasadig2013@gmail.com>
2025-08-18 12:01:30 +0300

    feat: applies rate limitting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ContentModule } from './content/content.module';
 import { DiscoveryModule } from './discovery/discovery.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,8 +16,20 @@ import { DiscoveryModule } from './discovery/discovery.module';
     DatabaseModule,
     ContentModule,
     DiscoveryModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,
+        limit: 2,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
