272e395d567f89f06acbd50c4bd69a96c736fe10
Y4h14 <yahyasadig2013@gmail.com>
2025-08-18 11:45:16 +0300

    fix: makes status route public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/public';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('')
+  @Public()
+  @Get('z')
   status() {
     return this.appService.status();
   }
